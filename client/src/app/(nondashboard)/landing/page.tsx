79980c63a541c5d16dc6c93d7e60e5f9136fedd7
Prabhat Kumar <prabhatsah349@gmail.com>
2025-07-04 18:22:24 +0530

    rtk implementtaion

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCarousel } from "@/hooks/useCarousel";
+import { useGetCoursesQuery } from "@/state/api";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
@@ -41,6 +42,11 @@ const LoadingSkeleton = () => {
 
 const Landing = () => {
   const currentImage = useCarousel({ totalImages: 3 });
+
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+  console.log("courses :", courses);
+  console.log(courses);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; 2024 CollabX. ALl Rights Reserved</p>
+      <p>&copy; 2024 CollabX. All Rights Reserved</p>
       <div className="footer__links">
         {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
           <Link
